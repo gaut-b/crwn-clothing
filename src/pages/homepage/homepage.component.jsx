8041bc71266e01fe0cf391fd956d352a1207bca0
Gautier <gautier.bayle@gmail.com>
2019-08-23 21:27:27 +0200

    updating app with shop pages, collection preview component and collection item component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import Directory from '../../components/directory/directory.component.jsx'
 import './homepage.styles.scss';
 
-const HomePage = ({ history }) => {
+const HomePage = () => {
 	return(
 		<div className = 'HomePage'>
-			<Directory history = {history} />
+			<Directory />
 		</div>
 	);
 }
