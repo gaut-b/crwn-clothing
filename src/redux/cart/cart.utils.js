40da5131492d0c8b7360a51a855970550bedf6fe
gaut-b <gautier.bayle@gmail.com>
2019-10-02 11:43:12 +0200

    added AddItem and removeItem functionnality

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -17,3 +17,24 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 	}
 
 };
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+	
+	const existingCartItem = cartItems.find( 
+		cartItem => cartItem.id === cartItemToRemove.id
+	)
+
+	if (existingCartItem.quantity > 1) {
+		
+		return cartItems.map(cartItem =>
+			(cartItem.id == cartItemToRemove.id) 
+				? {...cartItem, quantity: cartItem.quantity - 1}
+				: cartItem
+		);
+
+	} else {
+
+		return cartItems.filter( cartItem => cartItem.id !== cartItemToRemove.id);
+
+	}
+};
